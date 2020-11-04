1d70d40b23c96aacfff67d5c2f74cfbc5bf1ccfe
Adam Cassidy <adamcassidy011@gmail.com>
2020-11-04 17:00:03 -0500

    Add express, mongoose, and boilerplate server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(3000, () => {
+  console.log("Server started.");
+});
