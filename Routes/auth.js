f1b0cc757732456a792cadab12fbc8929bf594f0
Adam Cassidy <adamcassidy011@gmail.com>
2020-11-06 14:06:09 -0500

    Add basic dependencies, database, and auth
    To allow gets, posts, and logging in.

---
+++ Routes/auth.js
@@ -0,0 +1,53 @@
+const route = require("express").Router();
+const userModel = require("../Models/userModel");
+const bcrypt = require("bcrypt")
+
+route.post("/user", async (req, res) => {
+  try {
+    const password = await req.body.password
+    const hashedPassword = await bcrypt.hash(password, 10)
+      const tempUser = { name: req.body.name, password: hashedPassword, role: req.body.role }
+    const user = await userModel.create(tempUser);
+    if (!user) return res.status(400).send("Can't create user");
+    res.send(true);
+  } catch (err) {
+      res.status(500).send(err);
+  }
+});
+route
+  .put("/user", async (req, res) => {
+    try {
+    const {_id, name, password, role} = req.body
+    const user = await userModel.findByIdAndUpdate(_id);
+    if (!user) return res.status(400).send("User doesn't exist");
+    res.send("Updated user");
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  })
+  .post("/", async (req, res) => {
+    try {
+      const password = await req.body.password
+      const hashedPassword = await bcrypt.hash(password, 10)
+      const tempUser = { name: req.body.name, password: hashedPassword, role: req.body.role }
+    const user = await userModel.findOne(tempUser);
+      if (!user) return res.status(400).send("Incorrect email or password");
+      res.cookie = { 'user': user }
+      res.send(true)
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  })
+  .get("/", async (req, res) => {
+    try {
+
+    const users = await userModel.find()
+      if (!users) return res.status(400).send("No users");
+      res.send(users)
+    } catch (err) {
+      res.status(500).send(err);
+    }
+
+  });
+
+module.exports = route;
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
-mongoose.connect("mongodb://localhost", {
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
-app.listen(3000, () => {
-  console.log("Server started.");
+app.use("/auth", require("./Routes/auth.js"));
+
+app.listen(PORT, () => {
+  console.log("Server started on port: " + PORT);
 });
