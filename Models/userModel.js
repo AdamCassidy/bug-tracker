ec3690dc9ebe63069b71f10dd257285bfc9074f7
Adam Cassidy <adamcassidy011@gmail.com>
2020-11-10 22:35:22 -0500

    Add user model
    So users can have name email password and role.

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,10 +1,33 @@
-const { Schema } = require("mongoose");
 const mongoose = require("mongoose");
+const { isEmail } = require("validator");
+const bcrypt = require("bcrypt");
 
 const schema = mongoose.Schema({
-  name: String,
-  password: String,
-  role: String,
+  name: {
+    type: String,
+    required: [true, "Please enter a name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter a password"],
+    minlength: [6, "Please enter at least 6 characters"],
+  },
+  role: {
+    type: String,
+    required: [true, "Please enter a role"],
+  },
+});
+
+schema.pre("save", async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
 });
 
 const model = mongoose.model("User", schema);
